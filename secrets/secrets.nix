f3042e33dd09bfe1be483d8145efac46042aa6ff
Sean Outram <me@sean.cyou>
2026-01-23 00:50:28 +0000

    Rekey immich secret to allow user editing

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
+  sean = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHfrKqOTYvCejD7bcZeJH0InGyVIdLvYvPqqI91ssNqj";
   titus = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIf5GYnmoU0fZ6DdS1Wt2dV72Jfuyvf2pkKkEa+tOq1t";
 in
 {
-  "immich.age".publicKeys = [ titus ];
+  "immich.age".publicKeys = [ titus sean ];
 }
