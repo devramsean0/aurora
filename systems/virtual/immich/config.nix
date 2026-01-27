4860b7aa4dce30d27b8533ff3dffa2d35c710c28
Sean Outram <me@sean.cyou>
2026-01-27 10:11:20 +0000

    Add the network mount, fix mac addresses and set the media location

--- systems/virtual/immich/config.nix
+++ systems/virtual/immich/config.nix
@@ -13,20 +13,16 @@
     cifs-utils
   ];
 
-  #fileSystems."/mnt/library" = {
-  #  device = "//192.168.1.123/Volume2/Immich-Media";
-  #  fsType = "cifs";
-  #  options = let
-      # this line prevents hanging on network split
-  #    automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-  #    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
-  #};
+  fileSystems."/mnt/library" = {
+    device = "//192.168.1.123/Volume2/Immich-Media";
+    fsType = "nfs";
+  };
 
   services.immich = {
     enable =  true;
     port = 2283;
     openFirewall = true;
     host = "::";
+    mediaLocation = "/mnt/library";
   };
 }
