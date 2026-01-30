5d9509dce7a0ef696db22a337d9bb55d5596fc60
Sean Outram <me@sean.cyou>
2026-01-30 16:20:19 +0000

    Lucius: Nginx: Inmmich: Copy GFCIMMICH extra config

--- systems/servers/lucius/config.nix
+++ systems/servers/lucius/config.nix
@@ -101,9 +101,28 @@
         http3 = true;
         http3_hq = true;
 
+        extraConfig = """
+          client_max_body_size 5000M;
+          proxy_request_buffering off;
+          client_body_buffer_size 1024k;
+          
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+
+          proxy_http_version 1.1;
+          proxy_redirect off;
+
+          proxy_read_timeout 600s;
+          proxy_send_timeout 600s;
+          send_timeout 600s;
+        """;
+
         locations."/" = {
           recommendedProxySettings = true;
           proxyPass = "http://immich.tail28b34.ts.net:2283";
+          proxyWebsockets = true;
         };
       };
     };
