f821b37f77d33a17869bdb0cf28e19f8907afe24
Sean Outram <me@sean.cyou>
2026-04-23 20:17:18 +0100

    dormlan: Fix interface names and semi-colon

--- systems/servers/dormlan/config.nix
+++ systems/servers/dormlan/config.nix
@@ -18,10 +18,10 @@
 
   networking = {
     # WAN
-    interfaces.eth0.useDHCP = true;
+    interfaces.ens18.useDHCP = true;
 
     # LAN
-    interfaces.eth1.ipv4.addresses = [{
+    interfaces.ens19.ipv4.addresses = [{
       address = "192.168.1.1";
       prefixLength = 24;
     }];
@@ -29,13 +29,13 @@
     # NAT
     nat = {
       enable = true;
-      externalInterface = "eth0";
-      internalInterfaces = [ "eth1" ];
+      externalInterface = "ens18";
+      internalInterfaces = [ "ens19" ];
     };
 
     firewall = {
       enable = true;
-      trustedInterfaces = [ "eth1" ];
+      trustedInterfaces = [ "ens19" ];
     };
   };
 
@@ -44,13 +44,13 @@
     enable = true;
 
     settings = {
-      interface = "eth1"; # Listen on the downstream bridge
+      interface = "ens19"; # Listen on the downstream bridge
       domain-needed = true; # Force a domain part to forward upstream
       bogus-priv = true; # Don't forward to upstream if private dns resolution fails
 
 
       dhcp-range = "192.168.1.2,192.168.100.254,12h";
-      domain = "local=/localnet"
+      domain = "local=/localnet";
 
       dhcp-option = [
         "3,192.168.100.1"  # gateway
